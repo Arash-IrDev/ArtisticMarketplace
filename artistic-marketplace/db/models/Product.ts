4191fde6053ae60f31e32a1c5d2411f13d9a8e77
Arash-IrDev <125612877+Arash-IrDev@users.noreply.github.com>
2023-05-28 23:35:20 +0300

    Backend has been scaffolded!

---
+++ artistic-marketplace/db/models/Product.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  category: String,
+  price: Number,
+  currency: String,
+  image: {
+    src: String,
+    alt: String,
+  },
+  bestseller: Boolean,
+  featured: Boolean,
+  details: {
+    dimmentions: {
+      width: Number,
+      height: Number,
+    },
+    size: Number,
+    description: String,
+    recommendations: [String],
+  },
+});
+
+export const Product = mongoose.models.Product || mongoose.model('Product', ProductSchema);
---
+++ artistic-marketplace/pages/api/products/[id].ts
@@ -0,0 +1,54 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import dbConnect from '../../../db/dbConnect';
+import { Product } from '../../../db/models/Product';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    query: { id },
+    method,
+  } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case 'GET':
+      try {
+        const product = await Product.findById(id);
+        if (!product) {
+          return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: product });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case 'PUT':
+      try {
+        const product = await Product.findByIdAndUpdate(id, req.body, {
+          new: true,
+          runValidators: true,
+        });
+        if (!product) {
+          return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: product });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case 'DELETE':
+      try {
+        const deletedProduct = await Product.deleteOne({ _id: id });
+        if (deletedProduct.deletedCount === 0) {
+          return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ success: true, data: {} });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
