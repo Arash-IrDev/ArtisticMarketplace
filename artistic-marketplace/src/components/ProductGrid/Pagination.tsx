378d526361d9ac0c69ea95ac39f33591f3212517
Arash-IrDev <125612877+Arash-IrDev@users.noreply.github.com>
2023-05-31 05:16:53 +0300

    Pagination Successfully Finished!

--- artistic-marketplace/src/components/ProductGrid/Pagination.tsx
+++ artistic-marketplace/src/components/ProductGrid/Pagination.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './Pagination.module.css';
 
 type Props = {
     currentPage: number,
@@ -11,12 +12,21 @@ const Pagination: React.FC<Props> = ({ currentPage, totalPages, onPageChange }) 
 
     return (
         <nav aria-label="Page navigation">
-            <ul className="pagination">
+            <ul className={`px-0 text-center ${styles.pagination}`}>
+                <li className={`${styles.pageItem} ${currentPage === 1 ? styles.disabled : ''}`}
+                    onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}>
+                    <span className={styles.pageLink}>&lt;</span>
+                </li>
                 {pages.map(page => (
-                    <li key={page} className={`page-item ${page === currentPage ? 'active' : ''}`}>
-                        <button className="page-link" onClick={() => onPageChange(page)}>{page}</button>
+                    <li key={page} className={`${styles.pageItem} ${page === currentPage ? styles.active : ''}`}
+                        onClick={() => onPageChange(page)}>
+                        <span className={styles.pageLink}>{page}</span>
                     </li>
                 ))}
+                <li className={`${styles.pageItem} ${currentPage === totalPages ? styles.disabled : ''}`}
+                    onClick={() => currentPage < totalPages && onPageChange(currentPage + 1)}>
+                    <span className={styles.pageLink}>&gt;</span>
+                </li>
             </ul>
         </nav>
     );
